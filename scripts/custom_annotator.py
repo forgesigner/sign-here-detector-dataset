cc523ef1fa8ba0bd135a5880b66e8abc227a9de5
khram2003 <nikita.khramov@gmail.com>
2023-11-07 12:27:03 +0100

    deletion of points in annotator

--- scripts/custom_annotator.py
+++ scripts/custom_annotator.py
@@ -28,6 +28,8 @@ class ImageAnnotator:
         self.canvas.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set)
 
         self.master.bind("<Return>", self.save_coordinates_wrapper)
+        self.master.bind("<BackSpace>", self.delete_last_point)
+        self.master.bind("<Left>", self.prev_image)
 
         self.canvas.bind("<Button-1>", self.set_point)
         self.canvas.bind("<Configure>", self.center_image)
@@ -35,15 +37,53 @@ class ImageAnnotator:
         self.load_image()
 
     def load_image(self):
-        if self.image_index < len(self.image_paths):
+        if 0 <= self.image_index < len(self.image_paths):
             image_path = self.image_paths[self.image_index]
             self.current_image = Image.open(image_path)
             self.tk_image = ImageTk.PhotoImage(self.current_image)
-            self.coordinates = []
             self.center_image()
+            self.master.title(f"Annotating: {image_path}")  # Show the path in the window title
+            # Load existing annotations if any
+            self.load_annotations(image_path)
         else:
             messagebox.showinfo("Finished", "No more images to annotate.")
             self.master.quit()
+
+
+    def delete_last_point(self, event):
+        if self.coordinates:
+            self.coordinates.pop()
+            self.center_image()  # Redraw the image and points
+
+    def prev_image(self, event):
+        if self.image_index > 0:
+            self.image_index -= 1
+            self.coordinates = []  # Clear current points
+            self.load_image()  # Load the previous image
+
+    def load_annotations(self, image_path):
+        json_file_path = self.get_annotation_file_path(image_path)
+        if os.path.exists(json_file_path):
+            with open(json_file_path, 'r') as f:
+                data = json.load(f)
+                self.coordinates = data.get('centers', [])
+                self.redraw_points()  # Function to redraw points
+
+    def get_annotation_file_path(self, image_path):
+        annotation_dir = os.path.join(self.base_annotation_dir, self.get_last_directory(image_path))
+        json_file = os.path.splitext(os.path.basename(image_path))[0] + '.json'
+        return os.path.join(annotation_dir, json_file)
+
+    def redraw_points(self):
+        self.center_image()  # Clear the canvas and redraw the image
+        image_x = (self.canvas.winfo_width() - self.tk_image.width()) // 2
+        image_y = (self.canvas.winfo_height() - self.tk_image.height()) // 2
+        for coord in self.coordinates:
+            canvas_x = coord[0] + image_x
+            canvas_y = coord[1] + image_y
+            self.canvas.create_oval(
+                canvas_x - 5, canvas_y - 5, canvas_x + 5, canvas_y + 5, fill='red'
+            )
 
 
     def center_image(self, event=None):
@@ -82,9 +122,8 @@ class ImageAnnotator:
         # Translate event coordinates to image coordinates
         x = self.canvas.canvasx(event.x) - image_x
         y = self.canvas.canvasy(event.y) - image_y
-
         # Check if the click is within the bounds of the image
-        if 0 <= x < self.tk_image.width() and 0 <= y < self.tk_image.height():
+        if 0 <= x < self.tk_image.width() and 0 <= y < self.tk_image.height() and (int(x), int(y)) not in self.coordinates:
             self.coordinates.append((int(x), int(y)))
             # We draw the point relative to the canvas, not the image
             self.canvas.create_oval(
